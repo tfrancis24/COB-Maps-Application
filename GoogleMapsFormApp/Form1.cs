f4ecd3f78b483c14aae9cf32d6f9effc98087f27
Francis, Tim <timothy.s.francis@gmail.com>
2019-02-01 16:22:01 -0800

    Added tail method. Still needs to be debugged

--- GoogleMapsFormApp/Form1.cs
+++ GoogleMapsFormApp/Form1.cs
@@ -121,6 +121,7 @@ namespace GoogleMapsFormApp
 
             HeadCircle(sw);
             writeCircleLocations(sw, locationDataCityCount);
+            TailCircle(sw);
             MessageBox.Show("Printed");
 
 
@@ -303,7 +304,7 @@ namespace GoogleMapsFormApp
             }
         }
 
-          public void Tail(StreamWriter writer)
+        public void Tail(StreamWriter writer)
           {
                List<string> html = new List<string>
                {
@@ -346,6 +347,52 @@ namespace GoogleMapsFormApp
 
                html.ForEach(writer.WriteLine);
           }
+
+        public void TailCircle(StreamWriter writer)
+        {
+            List<string> html = new List<string>
+            {
+                "",
+                "",
+                "var cityCircle;",
+                "",
+                "function initialize() {",
+                "var mapOptions = {",
+
+                "zoom: 4,",
+                "center: new google.maps.LatLng(37.09024, -95.712891),",
+                "mapTypeId: google.maps.MapTypeId.ROADMAP",
+                "};",
+                "",
+                "var map = new google.maps.Map(document.getElementById('map-canvas'),",
+                "mapOptions);",
+                "",
+                "for (var city in citymap) {",
+                "var populationOptions = {",
+                "map: map,",
+                "position: citymap[city].center,",
+                "icon: {",
+                "path: google.maps.SymbolPath.CIRCLE,",
+                "scale: Math.sqrt(citymap[city].population)*3,",
+                "strokeColor: '#FF0000',",
+                "strokeOpacity: 0.8,",
+                "strokeWeight: 2,",
+                "fillColor: '#FF0000',",
+                "fillOpacity: 0.35",
+                "},",
+                "clickable: true,",
+                "};",
+                "cityCircle = new google.maps.Marker(populationOptions);",
+                "}",
+                "}",
+                "google.maps.event.addDomListener(window, 'load', initialize);",
+                "</script>",
+                "</body>",
+                "</html>"
+            };
+
+            html.ForEach(writer.WriteLine);
+        }
 
 
     }
