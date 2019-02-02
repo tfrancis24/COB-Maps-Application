5c4821b09980054fd7b7ba832e6d05e4a56f9327
Francis, Tim <timothy.s.francis@gmail.com>
2019-02-02 01:05:42 -0800

    Fixed bug where map wasnt loading. Had to change param in getElementByID method. Also added indentation to some of the lines in the html

--- GoogleMapsFormApp/Form1.cs
+++ GoogleMapsFormApp/Form1.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace GoogleMapsFormApp
 {
-    public partial class Form1 : Form
+     public partial class Form1 : Form
      {
           //Holds the API key
           public static string apiKey = "";
@@ -23,7 +23,7 @@ namespace GoogleMapsFormApp
           public List<Tuple<string, int, MapLocation>> locationDataCityCount = new List<Tuple<string, int, MapLocation>>();
 
           //Array of city count objects
-          public CityCount[] cityCount; 
+          public CityCount[] cityCount;
 
 
           //Holds path of the location input file. For testing only
@@ -83,16 +83,16 @@ namespace GoogleMapsFormApp
 
           private void mapButton_Click(object sender, EventArgs e)
           {
-               
+
                readCSV();
 
                //Saves file "MapTest" to the user's desktop
                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
                StreamWriter sw = new StreamWriter(desktopPath + "\\MapTest.html")
                {
-                   //This fixed the issue of  streamwriter randomly freezing the write file process
-                   //Flushing the output buffer to keep it from being full
-                   AutoFlush = true
+                    //This fixed the issue of  streamwriter randomly freezing the write file process
+                    //Flushing the output buffer to keep it from being full
+                    AutoFlush = true
                };
 
                //These three functions write parts of the HTML file
@@ -108,28 +108,28 @@ namespace GoogleMapsFormApp
           //When clicked, will map circles to a google map
           private void circleMapButton_Click(object sender, EventArgs e)
           {
-              readCSVCircleCount();
+               readCSVCircleCount();
 
 
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            StreamWriter sw = new StreamWriter(desktopPath + "\\MapTest.html")
-            {
-                //This fixed the issue of  streamwriter randomly freezing the write file process
-                //Flushing the output buffer to keep it from being full
-                AutoFlush = true
-            };
+               string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+               StreamWriter sw = new StreamWriter(desktopPath + "\\MapTest.html")
+               {
+                    //This fixed the issue of  streamwriter randomly freezing the write file process
+                    //Flushing the output buffer to keep it from being full
+                    AutoFlush = true
+               };
 
-            HeadCircle(sw);
-            writeCircleLocations(sw, locationDataCityCount);
-            TailCircle(sw);
-            MessageBox.Show("Printed");
+               HeadCircle(sw);
+               writeCircleLocations(sw, locationDataCityCount);
+               TailCircle(sw);
+               MessageBox.Show("Printed");
 
 
-        }
+          }
 
-        //Reads location data from an input CSV file
-        //This method will be used to map pins
-        public void readCSV() //for marker map only
+          //Reads location data from an input CSV file
+          //This method will be used to map pins
+          public void readCSV() //for marker map only
           {
                //Loops through lines in csv file, skip the header row 
                foreach (var line in File.ReadAllLines(inputPath, Encoding.GetEncoding(1250)).Skip(1))
@@ -146,13 +146,13 @@ namespace GoogleMapsFormApp
                          Region = addressInfo[4],
                          PostalCode = addressInfo[5]
                     });
-                      
+
                     //Adds the company name and the maplocation tied to it into the list
                     locationData.Add(new Tuple<string, MapLocation>(addressInfo[0], location));
 
-               }      
+               }
           }
-           
+
           //Reads from the input file to count number of cities on the map
           public void readCSVCircleCount()
           {
@@ -164,92 +164,62 @@ namespace GoogleMapsFormApp
 
                for (int i = 0; i < lines.Length; i++)
                {
-                   string[] addressInfo = lines[i].Split(','); //Splits each row of the data into an array
-                   string city = addressInfo[3];
-                   string state = addressInfo[4];
-                   bool found = false;
+                    string[] addressInfo = lines[i].Split(','); //Splits each row of the data into an array
+                    string city = addressInfo[3];
+                    string state = addressInfo[4];
+                    bool found = false;
 
-                   for (int j = 0; j < uniqueCityCount; j++)
-                   {
-                       if (cityCount[j].City == city && cityCount[j].State == state)
-                       {
-                           cityCount[j].Count++;
-                           found = true;
-                           break;
-                       }
-                   }
+                    for (int j = 0; j < uniqueCityCount; j++)
+                    {
+                         if (cityCount[j].City == city && cityCount[j].State == state)
+                         {
+                              cityCount[j].Count++;
+                              found = true;
+                              break;
+                         }
+                    }
 
-                   if (!found)
-                   {
-                       cityCount[uniqueCityCount] = new CityCount
-                       {
-                           City = city,
-                           State = state,
-                           Count = 1
-                       };
-                       uniqueCityCount++;
-                   }
+                    if (!found)
+                    {
+                         cityCount[uniqueCityCount] = new CityCount
+                         {
+                              City = city,
+                              State = state,
+                              Count = 1
+                         };
+                         uniqueCityCount++;
+                    }
                }
 
                //Remove null indexes
-               cityCount =  cityCount.Where(c => c != null).ToArray();
+               cityCount = cityCount.Where(c => c != null).ToArray();
 
                foreach (var cityCountObject in cityCount)
                {
-                   //MapLocation object is intialized with values from array
-                   MapLocation location = geocodeClient.GetMapLocation(new Address
-                   {
-                       Street = null,
-                       Apt = null, //null
-                       City = cityCountObject.City,
-                       Region = cityCountObject.State,
-                       PostalCode = null
+                    //MapLocation object is intialized with values from array
+                    MapLocation location = geocodeClient.GetMapLocation(new Address
+                    {
+                         Street = null,
+                         Apt = null, //null
+                         City = cityCountObject.City,
+                         Region = cityCountObject.State,
+                         PostalCode = null
 
-                   });
+                    });
 
-                   //Adds the company name and the maplocation tied to it into the list
-                   locationDataCityCount.Add(new Tuple<string, int, MapLocation>(cityCountObject.City + "," + 
-                       cityCountObject.State, cityCountObject.Count, location));
+                    //Adds the company name and the maplocation tied to it into the list
+                    locationDataCityCount.Add(new Tuple<string, int, MapLocation>(cityCountObject.City + "," +
+                        cityCountObject.State, cityCountObject.Count, location));
                }
 
 
           }
 
-          
+
           public void Head(StreamWriter writer)
           {
                //StreamWriter _writer = new StreamWriter("test.html");
                List<string> html = new List<string>
-               {
-                     "<!DOCTYPE html>",
-                     "<html>",
-                     "<head>",
-                     "  <meta http-equiv=\"content - type\" content=\"text/html; charset = UTF-8\" /> ",
-                     "  <title>Where Do They Come From?</title>",
-                     "    <style>",
-                     "      html, body, #map {",
-                     "        height: 100%;",
-                     "        margin: 0px;",
-                     "        padding: 0px",
-                     "      }",
-                     "    </style>",
-                     "  <script src=\"http://maps.google.com/maps/api/js?key=" + apiKey + "&callback=initMap\"", 
-                     "          type=\"text/javascript\"></script>",
-                     "</head>",
-                     "<body>",
-                     "  <div id=\"map\"></div>",
-                     "",
-                     "  <script type=\"text/javascript\">",
-                     "    var locations = ["
-               };
-
-               html.ForEach(writer.WriteLine);
-          }
-
-        public void HeadCircle(StreamWriter writer)
-        {
-            //StreamWriter _writer = new StreamWriter("test.html");
-            List<string> html = new List<string>
                {
                      "<!DOCTYPE html>",
                      "<html>",
@@ -270,41 +240,71 @@ namespace GoogleMapsFormApp
                      "  <div id=\"map\"></div>",
                      "",
                      "  <script type=\"text/javascript\">",
-                     "    var citymap = {};"
+                     "    var locations = ["
                };
 
-            html.ForEach(writer.WriteLine);
-        }
+               html.ForEach(writer.WriteLine);
+          }
+
+          public void HeadCircle(StreamWriter writer)
+          {
+               //StreamWriter _writer = new StreamWriter("test.html");
+               List<string> html = new List<string>
+               {
+                     "<!DOCTYPE html>",
+                     "<html>",
+                     "<head>",
+                     "  <meta http-equiv=\"content - type\" content=\"text/html; charset = UTF-8\" /> ",
+                     "  <title>Where Do They Come From?</title>",
+                     "    <style>",
+                     "      html, body, #map {",
+                     "        height: 100%;",
+                     "        margin: 0px;",
+                     "        padding: 0px",
+                     "      }",
+                     "    </style>",
+                     "  <script src=\"http://maps.google.com/maps/api/js?key=" + apiKey + "&callback=initMap\"",
+                     "          type=\"text/javascript\"></script>",
+                     "</head>",
+                     "<body>",
+                     "  <div id=\"map\"></div>",
+                     "",
+                     "  <script type=\"text/javascript\">",
+                     "var citymap = {};"
+               };
+
+               html.ForEach(writer.WriteLine);
+          }
 
           public void writeLocations(StreamWriter writer, List<Tuple<string, MapLocation>> locationData)
           {
-                for (int i = 0; i < locationData.Count; i++)
-                {
+               for (int i = 0; i < locationData.Count; i++)
+               {
                     if (i == locationData.Count - 1)
                     {
-                        //last element, no ending comma
-                        writer.Write($"		['{locationData[i].Item1}', {locationData[i].Item2.latitude}, {locationData[i].Item2.longitude}]\n");
+                         //last element, no ending comma
+                         writer.Write($"		['{locationData[i].Item1}', {locationData[i].Item2.latitude}, {locationData[i].Item2.longitude}]\n");
                     }
                     else
                     {
-                        writer.Write($"		['{locationData[i].Item1}', {locationData[i].Item2.latitude}, {locationData[i].Item2.longitude}],\n");
+                         writer.Write($"		['{locationData[i].Item1}', {locationData[i].Item2.latitude}, {locationData[i].Item2.longitude}],\n");
                     }
-                }
+               }
           }
 
-        public void writeCircleLocations(StreamWriter writer, List<Tuple<string, int, MapLocation>> locationData)
-        {
-            for (int i = 0; i < locationData.Count; i++)
-            {
-                writer.Write("citymap['" + locationData[i].Item3.City + "," + locationData[i].Item3.Region + "']= {\n"
-                    + "center: new google.maps.LatLng(" + locationData[i].Item3.latitude + ", " + locationData[i].Item3.longitude + "),\n"
-                    + "population: " + locationData[i].Item2 + ",\n"
-                    + "name:' " + locationData[i].Item3.City + ", " + locationData[i].Item3.Region + "'\n"
-                    + "};\n");
-            }
-        }
+          public void writeCircleLocations(StreamWriter writer, List<Tuple<string, int, MapLocation>> locationData)
+          {
+               for (int i = 0; i < locationData.Count; i++)
+               {
+                    writer.Write("	citymap['" + locationData[i].Item3.City + "," + locationData[i].Item3.Region + "']= {\n"
+                        + "		center: new google.maps.LatLng(" + locationData[i].Item3.latitude + ", " + locationData[i].Item3.longitude + "),\n"
+                        + "		population: " + locationData[i].Item2 + ",\n"
+                        + "		name:' " + locationData[i].Item3.City + ", " + locationData[i].Item3.Region + "'\n"
+                        + "	};\n");
+               }
+          }
 
-        public void Tail(StreamWriter writer)
+          public void Tail(StreamWriter writer)
           {
                List<string> html = new List<string>
                {
@@ -348,52 +348,51 @@ namespace GoogleMapsFormApp
                html.ForEach(writer.WriteLine);
           }
 
-        public void TailCircle(StreamWriter writer)
-        {
-            List<string> html = new List<string>
-            {
-                "",
-                "",
-                "var cityCircle;",
-                "",
-                "function initialize() {",
-                "var mapOptions = {",
+          public void TailCircle(StreamWriter writer)
+          {
+               List<string> html = new List<string>
+               {
+                     "",
+                     "",
+                     "var cityCircle;",
+                     "",
+                     "function initialize() {",
+                     "  var mapOptions = {",
+                     "    zoom: 4,",
+                     "    center: new google.maps.LatLng(37.09024, -95.712891),",
+                     "    mapTypeId: google.maps.MapTypeId.ROADMAP",
+                     "  };",
+                     "",
+                     "  var map = new google.maps.Map(document.getElementById('map'),",
+                     "      mapOptions);",
+                     "",
+                     "  for (var city in citymap) {",
+                     "    var populationOptions = {",
+                     "      map: map,",
+                     "      position: citymap[city].center,",
+                     "      icon: {",
+                     "      path: google.maps.SymbolPath.CIRCLE,",
+                     "      scale: Math.sqrt(citymap[city].population)*3,",
+                     "      strokeColor: '#FF0000',",
+                     "      strokeOpacity: 0.8,",
+                     "      strokeWeight: 2,",
+                     "      fillColor: '#FF0000',",
+                     "      fillOpacity: 0.35",
+                     "      },",
+                     "      clickable: true,",
+                     "    };",
+                     "    cityCircle = new google.maps.Marker(populationOptions);",
+                     "  }",
+                     "}",
+                     "google.maps.event.addDomListener(window, 'load', initialize);",
+                     "</script>",
+                     "</body>",
+                     "</html>"
+                };
 
-                "zoom: 4,",
-                "center: new google.maps.LatLng(37.09024, -95.712891),",
-                "mapTypeId: google.maps.MapTypeId.ROADMAP",
-                "};",
-                "",
-                "var map = new google.maps.Map(document.getElementById('map-canvas'),",
-                "mapOptions);",
-                "",
-                "for (var city in citymap) {",
-                "var populationOptions = {",
-                "map: map,",
-                "position: citymap[city].center,",
-                "icon: {",
-                "path: google.maps.SymbolPath.CIRCLE,",
-                "scale: Math.sqrt(citymap[city].population)*3,",
-                "strokeColor: '#FF0000',",
-                "strokeOpacity: 0.8,",
-                "strokeWeight: 2,",
-                "fillColor: '#FF0000',",
-                "fillOpacity: 0.35",
-                "},",
-                "clickable: true,",
-                "};",
-                "cityCircle = new google.maps.Marker(populationOptions);",
-                "}",
-                "}",
-                "google.maps.event.addDomListener(window, 'load', initialize);",
-                "</script>",
-                "</body>",
-                "</html>"
-            };
-
-            html.ForEach(writer.WriteLine);
-        }
+               html.ForEach(writer.WriteLine);
+          }
 
 
-    }
+     }
 }
