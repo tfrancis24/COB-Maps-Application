69810e07f3766f2259c501df083c2ec349902529
Isabella <leeisab@oregonstate.edu>
2019-02-13 17:41:51 -0800

    Update Form1.cs
    tested new .csv file. doesn't address the Anderson, SC, map, but replaces it with Independence, KS.

--- GoogleMapsFormApp/Form1.cs
+++ GoogleMapsFormApp/Form1.cs
@@ -27,7 +27,7 @@ namespace GoogleMapsFormApp
 
 
           //Holds path of the location input file. For testing only
-          public string inputPath = "BIS_Meet_the_Firms_02082019.csv";
+          public string inputPath = "COB_Meet_the_Firms_02152019.csv";
 
           public Form1()
           {
