b732a1290cf569d3429f155b2422087bc1514b47
Isabella <leeisab@oregonstate.edu>
2019-02-28 13:20:36 -0800

    updates
    removed API key, put back DSGN file

--- GoogleMapsFormApp/Form1.cs
+++ GoogleMapsFormApp/Form1.cs
@@ -11,7 +11,7 @@ namespace GoogleMapsFormApp
      public partial class Form1 : Form
      {
           //Holds the API key
-          public static string apiKey = "AIzaSyCPA9Iz-wpVr8-vwx4oL2n1fVG0IYmImwM";
+          public static string apiKey = "";
 
           //Geocode object - place the API key here
           public GeocodeClient geocodeClient = new GeocodeClient(apiKey);
@@ -28,7 +28,7 @@ namespace GoogleMapsFormApp
 
 
         //Holds path of the location input file. For testing only
-        public string inputPath = "COB-Maps-Application\\GoogleMapsFormApp\\LocationData\\Meet_the_DSGN_Firms_02222019.csv";
+        public string inputPath = "Meet_the_DSGN_Firms_02222019.csv";
         //public string systemPath = Path.Combine(Environment.CurrentDirectory, inputPath);
 
 
